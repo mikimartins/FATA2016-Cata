6789ae0ab97e71b67e99ebd86515d32489e51573
Thiago Hersan <thiago.hersan+github@gmail.com>
2016-05-17 11:13:21 -0300

    fixes git hook handler

--- gitHookler.php
+++ gitHookler.php
@@ -7,18 +7,16 @@ if ( $data['repository'] ) {
         shell_exec( 'cd /home/tgh/FATA2016-Cata && git reset --hard HEAD && git pull origin master' );
         shell_exec( 'cd /home/tgh/FATA2016-Cata && ./makeBook.py' );
         shell_exec( 'cp /home/tgh/FATA2016-Cata/xbook.pdf /home/tgh/WWW' );
+        echo "Book Ready";
     }
-    echo "Book Ready";
-}
-elseif ( $data['repository'] ) {
-    if( strcmp($data['repository']['full_name'], "thiagohersan/FATA2016-Site") == 0 ){
+    elseif( strcmp($data['repository']['full_name'], "thiagohersan/FATA2016-Site") == 0 ){
         echo "Generating Site";
         //shell_exec( 'cd /home/tgh/FATA2016-Site && git reset --hard HEAD && git pull origin master' );
         //shell_exec( 'cd /home/tgh/FATA2016-Site && hugo' );
         //shell_exec( 'cd /home/tgh/WWW && rm -rf FATA2016/*' );
         //shell_exec( 'cp -r /home/tgh/FATA2016-Cata/public/* /home/tgh/WWW/FATA2016/' );
+        echo "Site Ready";
     }
-    echo "Site Ready";
 }
 
 ?>
