f8b57593bfe62bdc59d0ab73596080055c45e8de
Thiago Hersan <thiago.hersan+github@gmail.com>
2016-06-02 21:08:11 -0300

    new dir structure for different kinds of texts

--- makeBook.py
+++ makeBook.py
@@ -2,11 +2,12 @@
 # -*- coding: utf-8 -*-
 
 from os import listdir, system
-from os.path import join
+from os.path import join, isdir
 from time import sleep
+from re import sub
 
 if __name__ == "__main__":
-    DATA_DIR = "."
+    DATA_DIR = "./texts"
     HTML_TEMPLATE = "template.html"
     BOOK_NAME = "xbook"
     TOC_TAG = "<!-- !!! TOCTOCTOC !!! -->"
@@ -15,45 +16,50 @@ if __name__ == "__main__":
     BODY = ""
     TOC = ""
 
-    TOC += "            <h3>Projetos</h3>\n"
-    TOC += "            <ul class=\"toc\">\n"
-
-    for idx,filename in enumerate([f for f in sorted(listdir(DATA_DIR)) if f.endswith(".txt")]):
-        fullPath = join(DATA_DIR, filename)
-        print "PROCESSING: %s"%fullPath
+    for txtDir in [f for f in sorted(listdir(DATA_DIR)) if isdir(join(DATA_DIR, f))]:
+        thisDir = join(DATA_DIR, txtDir)
+        thisHeading = sub(r"[0-9]+", "", txtDir).title()
         
-        cHtml = ""
-        cTitle = ""
+        TOC += "            <h3>%s</h3>\n"%thisHeading
+        TOC += "            <ul class=\"toc\">\n"
 
-        # expand the html and add to TOC list
-        with open(fullPath) as txt:
-            for line in txt.read().splitlines():
-                if cTitle is "":
-                    cTitle = line
-                    TOC += "				<li><a href=\"#ch%s\">%s</a></li>\n"%(str(idx), cTitle)
-                elif "images/" in line:
-                    if cHtml is "":
-                        cHtml += "        <div class=\"projcover\">\n"
-                        cHtml += "            <img src=%s />\n"%line
-                        cHtml += "            <h2>%s</h2>\n"%cTitle.replace(": ","<br />")
-                        cHtml += "        </div>\n"
-                        cHtml += "        <h1 id=\"ch%s\" class=\"chapter\">%s</h1>\n"%(str(idx), cTitle)
+
+        for idx,filename in enumerate([f for f in sorted(listdir(thisDir)) if f.endswith(".txt")]):
+            fullPath = join(thisDir, filename)
+            print "PROCESSING: %s"%fullPath
+
+            cHtml = ""
+            cTitle = ""
+
+            # expand the html and add to TOC list
+            with open(fullPath) as txt:
+                for line in txt.read().splitlines():
+                    if cTitle is "":
+                        cTitle = line
+                        TOC += "				<li><a href=\"#ch%s\">%s</a></li>\n"%(str(idx), cTitle)
+                    elif "images/" in line:
+                        if cHtml is "":
+                            cHtml += "        <div class=\"projcover\">\n"
+                            cHtml += "            <img src=%s />\n"%line
+                            cHtml += "            <h2>%s</h2>\n"%cTitle.replace(": ","<br />")
+                            cHtml += "        </div>\n"
+                            cHtml += "        <h1 id=\"ch%s\" class=\"chapter\">%s</h1>\n"%(str(idx), cTitle)
+                        else:
+                            cHtml += "        <div class=\"projcover\">\n"
+                            cHtml += "            <img src=%s />\n"%line
+                            cHtml += "        </div>\n"
                     else:
-                        cHtml += "        <div class=\"projcover\">\n"
-                        cHtml += "            <img src=%s />\n"%line
-                        cHtml += "        </div>\n"
-                else:
-                    cHtml += "        %s\n"%line
-            txt.close()
+                        cHtml += "        %s\n"%line
+                txt.close()
 
-        if cHtml is not "":
-            BODY += cHtml
+            if cHtml is not "":
+                BODY += cHtml
 
-    # close TOC
-    TOC += "            </ul>\n"
+        # close TOC
+        TOC += "            </ul>\n"
 
     # write output file
-    with open(join(DATA_DIR, BOOK_NAME+".html"), 'w') as out, open(join(DATA_DIR, HTML_TEMPLATE)) as temp:
+    with open(BOOK_NAME+".html", 'w') as out, open(HTML_TEMPLATE) as temp:
         for line in temp.readlines():
             if TOC_TAG in line:
                 line = TOC
